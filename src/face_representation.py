55b55676f6418847628cde791f46121322c484ea
RigvedManoj <rmanoj@umass.edu>
2024-10-14 20:26:58 -0400

    add face representation functionality

---
+++ src/face_representation.py
@@ -0,0 +1,37 @@
+import json
+
+from deepface import DeepFace
+
+
+# Function that takes in path to image and returns a list of face embeddings and corresponding region for all
+# faces in the image.
+def detect_faces_and_get_embeddings(image_path):
+
+    with open("../resources/model_config.json", "r") as config_file:
+        config = json.load(config_file)
+    model_name = config["model_name"]
+    detector_backend = config["detector_backend"]
+
+    results = DeepFace.represent(
+        image_path,
+        model_name=model_name,
+        detector_backend=detector_backend,
+        enforce_detection=True,
+    )
+    face_embeddings = []
+
+    for i, result in enumerate(results):
+        embedding = result["embedding"]
+
+        x, y, width, height = (
+            result["facial_area"]["x"],
+            result["facial_area"]["y"],
+            result["facial_area"]["w"],
+            result["facial_area"]["h"],
+        )
+
+        face_embeddings.append(
+            {"embedding": embedding, "region": (x, y, width, height)}
+        )
+
+    return face_embeddings
