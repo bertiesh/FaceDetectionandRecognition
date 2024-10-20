675cee55dc75bf200ccf417579c7e9fe93a82dcf
anjus <anjuskumar1313@gmail.com>
2024-10-20 16:22:51 -0400

    Corrected Formatting

--- test/test_fileCreationAndDataUpload.py
+++ test/test_fileCreationAndDataUpload.py
@@ -1,7 +1,8 @@
 import os
 import unittest
-from unittest.mock import patch
+
 import pandas as pd
+
 from src.facematch.bulk_upload import upload_embedding_to_database
 
 
@@ -10,27 +11,34 @@ class TestUploadEmbeddingToDatabase(unittest.TestCase):
 
         # Sample data for testing
         test_data = [
-            {"image_path": "test/data/img1.png", "embedding": [1,2,3], "bbox": [1,2,300,200]},
-            {"image_path": "test/data/img2.png", "embedding": [4,2,3], "bbox": [5,2,300,200]},
+            {
+                "image_path": "test/data/img1.png",
+                "embedding": [1, 2, 3],
+                "bbox": [1, 2, 300, 200],
+            },
+            {
+                "image_path": "test/data/img2.png",
+                "embedding": [4, 2, 3],
+                "bbox": [5, 2, 300, 200],
+            },
         ]
-        
+
         # Database file
         self.csv_file = "test/data/test_embeddings.csv"
-        
+
         # Upload data
         upload_embedding_to_database(test_data, self.csv_file)
 
         # Read data
         df = pd.read_csv(self.csv_file)
-        
-        # Assert that the header and data are written correctly
-        self.assertEqual(df.iloc[0,0], "test/data/img1.png")
-        self.assertEqual(df.iloc[0,1], "1,2,3")
-        self.assertEqual(df.iloc[0,2], "1,2,300,200")
 
+        # Assert that the header and data are written correctly
+        self.assertEqual(df.iloc[0, 0], "test/data/img1.png")
+        self.assertEqual(df.iloc[0, 1], "1,2,3")
+        self.assertEqual(df.iloc[0, 2], "1,2,300,200")
 
     def test_directory_creation(self):
-        
+
         self.csv_file = "test/data/test_embeddings.csv"
 
         # Remove the directory if it exists for the test
@@ -38,19 +46,22 @@ class TestUploadEmbeddingToDatabase(unittest.TestCase):
             os.remove(self.csv_file)
 
         # Call the function
-        data = [{"image_path": "test/data/img1.png", "embedding": [1,2,3], "bbox": (1,2,300,200)}]
+        data = [
+            {
+                "image_path": "test/data/img1.png",
+                "embedding": [1, 2, 3],
+                "bbox": (1, 2, 300, 200),
+            }
+        ]
         upload_embedding_to_database(data, self.csv_file)
 
         # Assert that the directory now exists
         self.assertTrue(os.path.exists(self.csv_file))
 
-
     def tearDown(self):
         # Clean up the temporary CSV file
         if os.path.exists(self.csv_file):
             os.remove(self.csv_file)
-        
-            
 
 
 if __name__ == "__main__":
