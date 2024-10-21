3f9cb35c8e909a440e94825b154dd0fefbe0d19c
anjus <anjuskumar1313@gmail.com>
2024-10-20 23:05:25 -0400

    Added Faiss indexing and similarity search feature

--- src/facematch/similarity_search.py
+++ src/facematch/similarity_search.py
@@ -1,3 +1,5 @@
+import faiss
+import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -29,5 +31,37 @@ def cosine_similarity_search(
 
     # Return the image paths corresponding to the top N similar vectors or vectors with similarity higher than threshold
     top_img_paths = results["image_path"].to_list()
+
+    return top_img_paths
+
+
+def cosine_similarity_search_faiss(
+    query_vector, database_filepath, top_n=None, threshold=None
+):
+    # Ensure at least one of top_n or threshold is set
+    if top_n is None and threshold is None:
+        raise ValueError("Either top_n or threshold must be specified.")
+
+    # Normalize the query vector
+    query_vector = query_vector / np.linalg.norm(query_vector)
+    query_vector = query_vector.astype("float32")
+    faiss_path = database_filepath.replace(".csv", ".bin")
+
+    # Load the faiss index
+    index = faiss.read_index(faiss_path)
+
+    if top_n:
+        distances, indices = index.search(query_vector.reshape(1, -1), top_n)
+        indices = indices[0]
+    else:
+        # Perform a Range Search
+        no_of_beighbours, distances, indices = index.range_search(
+            query_vector.reshape(1, -1), threshold
+        )
+        indices = indices.tolist()
+
+    df = pd.read_csv(database_filepath)
+
+    top_img_paths = [df["image_path"].iloc[i - 1] for i in indices if i != -1]
 
     return top_img_paths
--- test/test_fileCreationAndDataUpload.py
+++ test/test_fileCreationAndDataUpload.py
@@ -1,16 +1,16 @@
 import os
 import unittest
 
+import faiss
 import pandas as pd
 
 from src.facematch.database_functions import upload_embedding_to_database
 
 
 class TestUploadEmbeddingToDatabase(unittest.TestCase):
-    def test_upload_embedding_to_database(self):
-
+    def setUp(self):
         # Sample data for testing
-        test_data = [
+        self.test_data = [
             {
                 "image_path": "test/data/img1.png",
                 "embedding": [1, 2, 3],
@@ -26,8 +26,12 @@ class TestUploadEmbeddingToDatabase(unittest.TestCase):
         # Database file
         self.csv_file = "test/data/test_embeddings.csv"
 
+        # Construct the Faiss index path
+        self.faiss_path = self.csv_file.replace(".csv", ".bin")
+
+    def test_upload_embedding_to_database(self):
         # Upload data
-        upload_embedding_to_database(test_data, self.csv_file)
+        upload_embedding_to_database(self.test_data, self.csv_file)
 
         # Read data
         df = pd.read_csv(self.csv_file)
@@ -37,23 +41,24 @@ class TestUploadEmbeddingToDatabase(unittest.TestCase):
         self.assertEqual(df.iloc[0, 1], "1,2,3")
         self.assertEqual(df.iloc[0, 2], "1,2,300,200")
 
+    def test_faiss_index(self):
+        # Upload data
+        upload_embedding_to_database(self.test_data, self.csv_file)
+
+        # Assert that the Faiss index file was created
+        assert os.path.exists(self.faiss_path)
+
+        # Load the Faiss index and check the number of embeddings
+        index = faiss.read_index(self.faiss_path)
+
+        assert index.ntotal == len(self.test_data)
+
     def test_directory_creation(self):
-
-        self.csv_file = "test/data/test_embeddings.csv"
-
         # Remove the directory if it exists for the test
         if os.path.exists(self.csv_file):
             os.remove(self.csv_file)
 
-        # Call the function
-        data = [
-            {
-                "image_path": "test/data/img1.png",
-                "embedding": [1, 2, 3],
-                "bbox": (1, 2, 300, 200),
-            }
-        ]
-        upload_embedding_to_database(data, self.csv_file)
+        upload_embedding_to_database(self.test_data, self.csv_file)
 
         # Assert that the directory now exists
         self.assertTrue(os.path.exists(self.csv_file))
@@ -62,6 +67,10 @@ class TestUploadEmbeddingToDatabase(unittest.TestCase):
         # Clean up the temporary CSV file
         if os.path.exists(self.csv_file):
             os.remove(self.csv_file)
+
+        # Clean up the temporary FAISS file
+        if os.path.exists(self.faiss_path):
+            os.remove(self.faiss_path)
 
 
 if __name__ == "__main__":
