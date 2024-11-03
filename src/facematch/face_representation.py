b95326d204bfed4fe080ca2e68d87540a51876f0
RigvedManoj <rmanoj@umass.edu>
2024-11-03 11:42:22 -0500

    add comments and logs

--- src/facematch/face_representation.py
+++ src/facematch/face_representation.py
@@ -5,39 +5,36 @@ from deepface import DeepFace
 from src.facematch.resource_path import get_resource_path
 
 
-# Function that takes in path to image and returns a list of face embeddings and corresponding region for all
-# faces in the image.
-def detect_faces_and_get_embeddings(image_path):
-    config_path = get_resource_path("model_config.json")
-    with open(config_path, "r") as config_file:
-        config = json.load(config_file)
-    model_name = config["model_name"]
-    detector_backend = config["detector_backend"]
-
-    results = DeepFace.represent(
-        image_path,
-        model_name=model_name,
-        detector_backend=detector_backend,
-        enforce_detection=True,
-    )
-    face_embeddings = []
-
-    for i, result in enumerate(results):
-        embedding = result["embedding"]
-
-        x, y, width, height = (
-            result["facial_area"]["x"],
-            result["facial_area"]["y"],
-            result["facial_area"]["w"],
-            result["facial_area"]["h"],
+# Function that takes in path to image and returns a status field and a list of face embeddings and corresponding
+# region for all faces in the image.
+def detect_faces_and_get_embeddings(image_path, model_name, detector_backend):
+    try:
+        results = DeepFace.represent(
+            image_path,
+            model_name=model_name,
+            detector_backend=detector_backend,
+            enforce_detection=True,
         )
+        face_embeddings = []
 
-        face_embeddings.append(
-            {
-                "image_path": image_path,
-                "embedding": embedding,
-                "bbox": [x, y, width, height],
-            }
-        )
+        for i, result in enumerate(results):
+            embedding = result["embedding"]
 
-    return face_embeddings
+            x, y, width, height = (
+                result["facial_area"]["x"],
+                result["facial_area"]["y"],
+                result["facial_area"]["w"],
+                result["facial_area"]["h"],
+            )
+
+            face_embeddings.append(
+                {
+                    "image_path": image_path,
+                    "embedding": embedding,
+                    "bbox": [x, y, width, height],
+                }
+            )
+
+        return True, face_embeddings
+    except Exception as e:
+        return False, []
--- src/facematch/interface.py
+++ src/facematch/interface.py
@@ -1,54 +1,86 @@
 import json
 import os
+import logger
 
 from src.facematch.database_functions import upload_embedding_to_database
 from src.facematch.face_representation import detect_faces_and_get_embeddings
 from src.facematch.resource_path import get_resource_path
-from src.facematch.similarity_search import euclidean_distance_search_faiss
+from src.facematch.similarity_search import euclidean_distance_search_faiss, cosine_similarity_search
 
 
 class FaceMatchModel:
     # Function that takes in path to directory of images to upload to database and returns a success or failure message.
     def bulk_upload(self, image_directory_path, database_path=None):
         try:
+            # Get database from config file.
             if database_path is None:
                 config_path = get_resource_path("db_config.json")
                 with open(config_path, "r") as config_file:
                     config = json.load(config_file)
                 database_path = get_resource_path(config["database_path"])
 
+            # Get models from config file.
+            config_path = get_resource_path("model_config.json")
+            with open(config_path, "r") as config_file:
+                config = json.load(config_file)
+            model_name = config["model_name"]
+            detector_backend = config["detector_backend"]
+
+            # Call helper functions for each image file.
+            total_files_uploaded = 0
             embedding_outputs = []
             for filename in os.listdir(image_directory_path):
                 image_path = os.path.join(image_directory_path, filename)
                 if filename.lower().endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp")):
-                    embedding_outputs.extend(
-                        detect_faces_and_get_embeddings(image_path)
-                    )
+                    status, value = detect_faces_and_get_embeddings(image_path, model_name, detector_backend)
+                    if status:
+                        total_files_uploaded += 1
+                        embedding_outputs.extend(value)
+                        logger.log_info("Successfully uploaded file " + str(total_files_uploaded) + " to database")
 
             upload_embedding_to_database(embedding_outputs, database_path)
 
-            return "Successfully uploaded to database"
+            return "Successfully uploaded " + str(total_files_uploaded) + " files to database"
         except Exception as e:
             return f"An error occurred: {str(e)}"
 
     # Function that takes in path to image and returns all images that have the same person.
-    def find_face(self, image_file_path, database_path=None):
+    def find_face(self, image_file_path, database_path=None, toggle_faiss=True):
         try:
+            # Get database from config file.
             if database_path is None:
                 config_path = get_resource_path("db_config.json")
                 with open(config_path, "r") as config_file:
                     config = json.load(config_file)
                 database_path = get_resource_path(config["database_path"])
+
+            # Get models from config file.
+            config_path = get_resource_path("model_config.json")
+            with open(config_path, "r") as config_file:
+                config = json.load(config_file)
+            model_name = config["model_name"]
+            detector_backend = config["detector_backend"]
+            threshold = config["cosine-threshold"]
+
+            # Call helper functions to get similar images.
             filename = os.path.basename(image_file_path)
             if filename.lower().endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp")):
-                embedding_outputs = detect_faces_and_get_embeddings(image_file_path)
+                status, embedding_outputs = detect_faces_and_get_embeddings(image_file_path, model_name,
+                                                                            detector_backend)
                 matching_image_paths = []
-                for embedding_output in embedding_outputs:
-                    output = euclidean_distance_search_faiss(
-                        embedding_output["embedding"], database_path, threshold=17
-                    )
-                    matching_image_paths.extend(output)
-                return matching_image_paths
+                if status:
+                    for embedding_output in embedding_outputs:
+                        if toggle_faiss:
+                            output = euclidean_distance_search_faiss(
+                                embedding_output["embedding"], database_path, threshold=17)
+                        else:
+                            output = cosine_similarity_search(embedding_output["embedding"], database_path,
+                                                              threshold=threshold
+                                                              )
+                        matching_image_paths.extend(output)
+                    return matching_image_paths
+                else:
+                    return "Error: Provided image does not have any face"
             else:
                 return "Error: Provided file is not of image type"
         except Exception as e:
