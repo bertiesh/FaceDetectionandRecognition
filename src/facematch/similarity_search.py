d86ccee104103fb4dabae9e76f765af0665f92fb
anjus <anjuskumar1313@gmail.com>
2024-11-04 11:19:59 -0500

    Added cosine similarity for faiss with threshold using top-n

--- src/facematch/similarity_search.py
+++ src/facematch/similarity_search.py
@@ -3,14 +3,10 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
+from src.facematch.logger import log_debug
 
-def cosine_similarity_search(
-    query_vector, database_filepath, top_n=None, threshold=None
-):
-    # Ensure at least one of top_n or threshold is set
-    if top_n is None and threshold is None:
-        raise ValueError("Either top_n or threshold must be specified.")
 
+def cosine_similarity_search(query_vector, database_filepath, top_n=5, threshold=None):
     csv_file = database_filepath
 
     # Read the CSV file into a DataFrame and Embeddings stored as str to lists
@@ -22,12 +18,14 @@ def cosine_similarity_search(
         lambda x: cosine_similarity([x], [query_vector])[0][0]
     )
 
+    # Get top n based on similarity in descending order
+    results = df.nlargest(top_n, "similarity")
+
+    log_debug(results[["image_path", "embedding", "similarity"]])
+
     if threshold is not None:
         # Filter the DataFrame based on the threshold
-        results = df[df["similarity"] >= threshold]
-    else:
-        # Sort by similarity in descending order and get the top n
-        results = df.nlargest(top_n, "similarity")
+        results = results[results["similarity"] >= threshold]
 
     # Return the image paths corresponding to the top N similar vectors or vectors with similarity higher than threshold
     top_img_paths = results["image_path"].to_list()
@@ -36,12 +34,8 @@ def cosine_similarity_search(
 
 
 def cosine_similarity_search_faiss(
-    query_vector, database_filepath, top_n=None, threshold=None
+    query_vector, database_filepath, top_n=5, threshold=None
 ):
-    # Ensure at least one of top_n or threshold is set
-    if top_n is None and threshold is None:
-        raise ValueError("Either top_n or threshold must be specified.")
-
     # Normalize the query vector
     query_vector = query_vector / np.linalg.norm(query_vector)
     query_vector = query_vector.astype("float32")
@@ -50,19 +44,30 @@ def cosine_similarity_search_faiss(
     # Load the faiss index
     index = faiss.read_index(faiss_path)
 
-    if top_n:
-        distances, indices = index.search(query_vector.reshape(1, -1), top_n)
-        indices = indices[0]
-    else:
-        # Perform a Range Search
-        no_of_beighbours, distances, indices = index.range_search(
-            query_vector.reshape(1, -1), threshold
-        )
-        indices = indices.tolist()
+    distances, indices = index.search(query_vector.reshape(1, -1), top_n)
+    indices = indices[0]
+    indices = indices.tolist()
 
     df = pd.read_csv(database_filepath)
 
-    top_img_paths = [df["image_path"].iloc[i - 1] for i in indices if i != -1]
+    results = df.iloc[[i for i in indices if i != -1]]
+
+    results["embedding"] = results["embedding"].apply(
+        lambda x: list(map(float, x.split(",")))
+    )
+
+    results["similarity"] = results["embedding"].apply(
+        lambda x: cosine_similarity([x], [query_vector])[0][0]
+    )
+
+    log_debug(results[["image_path", "embedding", "similarity"]])
+
+    if threshold is not None:
+        # Filter the DataFrame based on the threshold
+        results = results[results["similarity"] >= threshold]
+
+    # Return the image paths corresponding to the top N similar vectors or vectors with similarity higher than threshold
+    top_img_paths = results["image_path"].to_list()
 
     return top_img_paths
 
