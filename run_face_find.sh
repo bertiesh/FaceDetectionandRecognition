075d5c4420448a78ad59efb01ae5dcc10ec37620
RigvedManoj <rmanoj@umass.edu>
2024-11-11 11:20:02 -0500

    add bash script to performance test face find function

---
+++ run_face_find.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export PYTHONPATH=$(pwd)
+
+# Start the Python server in the background
+python src/facematch/face_match_server.py &
+server_pid=$!
+echo "Server started with PID $server_pid"
+
+# Wait for the server to start (adjust time as necessary)
+sleep 10
+
+# Start timer
+start_time=$(date +%s)
+
+# Run your functions or any additional Python commands here
+python src/Sample_Client/sample_find_face_client.py "\path\to\input\image"
+
+# Calculate total time taken
+end_time=$(date +%s)
+total_time=$((end_time - start_time))
+
+# Stop the server
+kill $server_pid
+echo "Server stopped"
+
+# Print total time taken
+echo "Total time taken: $total_time seconds"
+
+read -p "Press any key to exit..."
