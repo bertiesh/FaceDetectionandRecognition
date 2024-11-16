c557ab6c7016b126f1e6e5e61b84b68026b073b7
anjus <anjuskumar1313@gmail.com>
2024-11-16 15:52:10 -0500

    Correction to normalize vectors before creaitng faiss index

--- src/facematch/FAISS.py
+++ src/facematch/FAISS.py
@@ -1,6 +1,7 @@
 import os
 
 import faiss
+import numpy as np
 
 
 def add_embeddings_faiss_index(embeddings, database_filepath):
@@ -9,6 +10,10 @@ def add_embeddings_faiss_index(embeddings, database_filepath):
     """
     # Create a new path with the ".bin" extension
     faiss_path = database_filepath.replace(".csv", ".bin")
+
+    # Normalize the embeddings
+    embeddings = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
+    embeddings = embeddings.astype("float32")
 
     if not os.path.exists(faiss_path):
         os.makedirs(os.path.dirname(faiss_path), exist_ok=True)
