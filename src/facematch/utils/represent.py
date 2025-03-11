a3f50bded832ed3803d57ac2dee35749ec6ae0c1
bertiesh <bertiesh98@gmail.com>
2025-03-11 15:36:52 -0400

    remove dependency fixes #32

--- src/facematch/utils/represent.py
+++ src/facematch/utils/represent.py
@@ -1,15 +1,108 @@
 # built-in dependencies
-from typing import Any, Dict, List, Union, Optional
+from typing import Any, Dict, List, Union, Optional, Tuple
 
 # 3rd party dependencies
 import numpy as np
+import cv2
 
 # project dependencies
-from deepface.commons import image_utils
-from deepface.modules import modeling, detection, preprocessing
+from src.facematch.utils import image_utils
+from deepface.modules import detection
 import onnxruntime as ort
 from src.facematch.utils.logger import log_info
 
+from tensorflow.keras.preprocessing import image
+
+def resize_image(img: np.ndarray, target_size: Tuple[int, int]) -> np.ndarray:
+    """
+    Resize an image to expected size of a ml model with adding black pixels.
+    Args:
+        img (np.ndarray): pre-loaded image as numpy array
+        target_size (tuple): input shape of ml model
+    Returns:
+        img (np.ndarray): resized input image
+    """
+    factor_0 = target_size[0] / img.shape[0]
+    factor_1 = target_size[1] / img.shape[1]
+    factor = min(factor_0, factor_1)
+
+    dsize = (
+        int(img.shape[1] * factor),
+        int(img.shape[0] * factor),
+    )
+    img = cv2.resize(img, dsize)
+
+    diff_0 = target_size[0] - img.shape[0]
+    diff_1 = target_size[1] - img.shape[1]
+
+    # Put the base image in the middle of the padded image
+    img = np.pad(
+        img,
+        (
+            (diff_0 // 2, diff_0 - diff_0 // 2),
+            (diff_1 // 2, diff_1 - diff_1 // 2),
+            (0, 0),
+        ),
+        "constant",
+    )
+
+    # double check: if target image is not still the same size with target.
+    if img.shape[0:2] != target_size:
+        img = cv2.resize(img, target_size)
+
+    # make it 4-dimensional how ML models expect
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+
+    if img.max() > 1:
+        img = (img.astype(np.float32) / 255.0).astype(np.float32)
+
+    return img
+
+def normalize_input(img: np.ndarray, normalization: str = "base") -> np.ndarray:
+    """Normalize input image.
+
+    Args:
+        img (numpy array): the input image.
+        normalization (str, optional): the normalization technique. Defaults to "base",
+        for no normalization.
+
+    Returns:
+        numpy array: the normalized image.
+    """
+
+    # issue 131 declares that some normalization techniques improves the accuracy
+
+    if normalization == "base":
+        return img
+
+    # @trevorgribble and @davedgd contributed this feature
+    # restore input in scale of [0, 255] because it was normalized in scale of
+    # [0, 1] in preprocess_face
+    img *= 255
+
+    if normalization == "raw":
+        pass  # return just restored pixels
+
+    elif normalization == "Facenet":
+        mean, std = img.mean(), img.std()
+        img = (img - mean) / std
+
+    elif normalization == "Facenet2018":
+        # simply / 127.5 - 1 (similar to facenet 2018 model preprocessing step as @iamrishab posted)
+        img /= 127.5
+        img -= 1
+
+    elif normalization == "ArcFace":
+        # Reference study: The faces are cropped and resized to 112×112,
+        # and each pixel (ranged between [0, 255]) in RGB images is normalised
+        # by subtracting 127.5 then divided by 128.
+        img -= 127.5
+        img /= 128
+    else:
+        raise ValueError(f"unimplemented normalization type - {normalization}")
+
+    return img
 
 def represent(
     img_path: Union[str, np.ndarray],
@@ -30,8 +123,7 @@ def represent(
             or a base64 encoded image. If the source image contains multiple faces, the result will
             include information for each detected face.
 
-        model_name (str): Model for face recognition. Options: VGG-Face, Facenet, Facenet512,
-            OpenFace, DeepFace, DeepID, Dlib, ArcFace, SFace and GhostFaceNet
+        model_name (str): Model for face recognition. Options: Facenet512, ArcFace, SFace and GhostFaceNet
 
         enforce_detection (boolean): If no face is detected in an image, raise an exception.
             Default is True. Set to False to avoid the exception for low-resolution images.
@@ -86,6 +178,7 @@ def represent(
     # log_info(f"target_size: {target_size}" + target_size.shape)
     target_size = ort_session.get_inputs()[0].shape[1:3]
     log_info(f"target_size: {target_size}")
+
     try:
         if detector_backend != "skip":
             img_objs = detection.extract_faces(
@@ -100,8 +193,7 @@ def represent(
         else:  # skip
             # Try load. If load error, will raise exception internal
             img, _ = image_utils.load_image(img_path)
-
-            log_info(f"img.shape: {img.shape}")
+            log_info(f"I was here. img: {img.shape}")
             if len(img.shape) != 3:
                 raise ValueError(f"Input img must be 3 dimensional but it is {img.shape}")
 
@@ -113,10 +205,12 @@ def represent(
                     "confidence": 0,
                 }
             ]
+        # log_info(f"img_objs: {img_objs}")
     except Exception as e:
         log_info(f"Face detection failed {str(e)}")
         raise ValueError(f"Face detection failed {str(e)}")
     # ---------------------------------
+
 
     if max_faces is not None and max_faces < len(img_objs):
         # sort as largest facial areas come first
@@ -139,20 +233,29 @@ def represent(
         region = img_obj["facial_area"]
         confidence = img_obj["confidence"]
 
-        # resize to expected shape of ml model
-        img = preprocessing.resize_image(
-            img=img,
-            # thanks to DeepId (!)
-            target_size=(target_size[1], target_size[0]),
-        )
-
-        # custom normalization
-        img = preprocessing.normalize_input(img=img, normalization=normalization)
+        try:
+            # resize to expected shape of ml model
+            img = resize_image(
+                img=img,
+                # thanks to DeepId (!)
+                target_size=(target_size[1], target_size[0]),
+            )
+            
+            # custom normalization
+            img = normalize_input(img=img, normalization=normalization)
+        except Exception as e:
+            log_info(f"Failed to resize or normalize image: {str(e)}")
 
         # embedding = model.forward(img)
         input_name = ort_session.get_inputs()[0].name
-        result = ort_session.run(None, {input_name: img})
-        embedding = result[0]
+        try:
+            result = ort_session.run(None, {input_name: img})
+        except Exception as e:
+            log_info(f"Failed to run inference: {str(e)}")
+        # log_info(f"Result: {result[0]}")
+        embedding = result[0].flatten()
+        log_info(f"Embedding shape: {embedding.shape}")
+
 
         resp_objs.append(
             {
@@ -161,5 +264,5 @@ def represent(
                 "face_confidence": confidence,
             }
         )
-
+        # log_info(f"resp_objs: {resp_objs}")
     return resp_objs
